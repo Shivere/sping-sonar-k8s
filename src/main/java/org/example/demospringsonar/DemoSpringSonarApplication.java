d9ecb7312930e4c5a9ad9866baddeec135b5ab24
Shivere <shawn.shivere@zeraki.co.ke>
2024-09-03 16:26:32 +0300

    first commit

---
+++ src/main/java/org/example/demospringsonar/DemoSpringSonarApplication.java
@@ -0,0 +1,13 @@
+package org.example.demospringsonar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringSonarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringSonarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/demospringsonar/DemoSpringSonarApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demospringsonar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringSonarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
